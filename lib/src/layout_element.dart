260da2f9b6d74039f50e1af334ec1a022bc7b992
CoMatu <30466272+CoMatu@users.noreply.github.com>
2021-02-09 21:17:38 +0500

    fixed ll

--- lib/src/layout_element.dart
+++ lib/src/layout_element.dart
@@ -5,7 +5,6 @@ import 'package:flutter_html/html_parser.dart';
 import 'package:flutter_html/src/html_elements.dart';
 import 'package:flutter_html/src/styled_element.dart';
 import 'package:flutter_html/style.dart';
-import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 import 'package:html/dom.dart' as dom;
 
 /// A [LayoutElement] is an element that breaks the normal Inline flow of
@@ -21,7 +20,7 @@ abstract class LayoutElement extends StyledElement {
   Widget toWidget(RenderContext context);
 }
 
-class TableLayoutElement extends LayoutElement {
+/* class TableLayoutElement extends LayoutElement {
   TableLayoutElement({
     String name,
     Style style,
@@ -150,7 +149,7 @@ class TableLayoutElement extends LayoutElement {
     );
   }
 }
-
+ */
 class TableSectionLayoutElement extends LayoutElement {
   TableSectionLayoutElement({
     String name,
@@ -263,11 +262,15 @@ class DetailsContentElement extends LayoutElement {
 
   @override
   Widget toWidget(RenderContext context) {
-    List<InlineSpan> childrenList = children?.map((tree) => context.parser.parseTree(context, tree))?.toList();
+    List<InlineSpan> childrenList = children
+        ?.map((tree) => context.parser.parseTree(context, tree))
+        ?.toList();
     List<InlineSpan> toRemove = [];
     if (childrenList != null) {
       for (InlineSpan child in childrenList) {
-        if (child is TextSpan && child.text != null && child.text.trim().isEmpty) {
+        if (child is TextSpan &&
+            child.text != null &&
+            child.text.trim().isEmpty) {
           toRemove.add(child);
         }
       }
@@ -275,29 +278,38 @@ class DetailsContentElement extends LayoutElement {
         childrenList.remove(child);
       }
     }
-    InlineSpan firstChild = childrenList?.isNotEmpty == true ? childrenList.first : null;
+    InlineSpan firstChild =
+        childrenList?.isNotEmpty == true ? childrenList.first : null;
     return ExpansionTile(
         expandedAlignment: Alignment.centerLeft,
-        title: elementList?.isNotEmpty == true && elementList?.first?.localName == "summary" ? StyledText(
-          textSpan: TextSpan(
-            style: style.generateTextStyle(),
-            children: [firstChild] ?? [],
-          ),
-          style: style,
-        ) : Text("Details"),
+        title: elementList?.isNotEmpty == true &&
+                elementList?.first?.localName == "summary"
+            ? StyledText(
+                textSpan: TextSpan(
+                  style: style.generateTextStyle(),
+                  children: [firstChild] ?? [],
+                ),
+                style: style,
+              )
+            : Text("Details"),
         children: [
           StyledText(
             textSpan: TextSpan(
-              style: style.generateTextStyle(),
-              children: getChildren(childrenList, context, elementList?.isNotEmpty == true && elementList?.first?.localName == "summary" ? firstChild : null)
-            ),
+                style: style.generateTextStyle(),
+                children: getChildren(
+                    childrenList,
+                    context,
+                    elementList?.isNotEmpty == true &&
+                            elementList?.first?.localName == "summary"
+                        ? firstChild
+                        : null)),
             style: style,
           ),
-        ]
-    );
+        ]);
   }
 
-  List<InlineSpan> getChildren(List<InlineSpan> children, RenderContext context, InlineSpan firstChild) {
+  List<InlineSpan> getChildren(
+      List<InlineSpan> children, RenderContext context, InlineSpan firstChild) {
     if (children == null) {
       return [];
     } else {
@@ -315,8 +327,8 @@ class EmptyLayoutElement extends LayoutElement {
 }
 
 LayoutElement parseLayoutElement(
-    dom.Element element,
-    List<StyledElement> children,
+  dom.Element element,
+  List<StyledElement> children,
 ) {
   switch (element.localName) {
     case "details":
@@ -327,15 +339,14 @@ LayoutElement parseLayoutElement(
           node: element,
           name: element.localName,
           children: children,
-          elementList: element.children
-      );
-    case "table":
+          elementList: element.children);
+/*     case "table":
       return TableLayoutElement(
         name: element.localName,
         children: children,
         node: element,
       );
-      break;
+      break; */
     case "thead":
     case "tbody":
     case "tfoot":
@@ -352,6 +363,6 @@ LayoutElement parseLayoutElement(
       );
       break;
     default:
-      return TableLayoutElement(children: children);
+      return null;
   }
 }
